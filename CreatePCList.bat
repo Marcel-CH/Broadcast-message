eaf03a1cb0915c32907c1c08c18087dea89081b2
Marcel-CH <_gucky_@gmx.ch>
2016-12-09 14:02:21 +0100

    Create CreatePCList.bat

---
+++ CreatePCList.bat
@@ -0,0 +1,4 @@
+Start "WLAN4" C:\Temp\GetComputerList\LAN4.bat /k
+Start "WLAN5" C:\Temp\GetComputerList\LAN5.bat /k
+Start "WLAN28" C:\Temp\GetComputerList\LAN28.bat /k
+Start "WLAN29" C:\Temp\GetComputerList\LAN29.bat /k
